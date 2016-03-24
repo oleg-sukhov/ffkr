15df32e1510bbbe4f11faa569950bf06bfec64ec
oleg-sukhov <oleg.sukhov@email.ua>
2016-03-24 23:44:40 +0200

    FFKR-0000: Add Navbar component

--- ffkr-ui/src/main/resources/ua/vn/ffkr/public/assets/js/src/ffkr.jsx
+++ ffkr-ui/src/main/resources/ua/vn/ffkr/public/assets/js/src/ffkr.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navbar from './components/common/navbar.jsx';
 
 class Home extends React.Component {
 
     render() {
-        return <div><h1>This is home component</h1></div>
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        );
     }
 }
 
